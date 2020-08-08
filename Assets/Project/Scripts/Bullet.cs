a266d32abf5f31f9bf741be0acf02565c8ad49e3
Makar Baderko <makarbaderko@gmail.com>
2020-08-08 16:41:56 +0300

    Added vectorized bullet moving, ddela timed bullet deleted

--- Assets/Project/Scripts/Bullet.cs
+++ Assets/Project/Scripts/Bullet.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public float speed = 8f;
+    public float lifeDuration = 0.7f;
+
+    private float lifeTimer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        lifeTimer = lifeDuration;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Make the bullet move
+        this.transform.position += this.transform.forward * speed * Time.deltaTime;
+        //Check if the byllet should be destroyed
+        lifeTimer -= Time.deltaTime;
+        if (lifeTimer <= 0f)
+		{
+            Destroy(this.gameObject);
+		}
     }
 }
